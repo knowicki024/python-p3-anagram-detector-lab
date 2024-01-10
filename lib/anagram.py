8df8ed83f87a09760d7560eb350273f4c4b68560
Katie N <146477745+knowicki024@users.noreply.github.com>
2024-01-10 18:27:31 -0500

    lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, words):
+        return [word for word in words if self.is_anagram(word)]
+    
+    def is_anagram(self, other_word):
+        other_word = other_word.lower()
+        return other_word != self.word and sorted(other_word) == self.sorted_word
+    
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
